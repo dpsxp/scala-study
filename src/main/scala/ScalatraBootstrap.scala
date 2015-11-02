ea50f1725fc97ccad0b287c92af42f2dc577885e
Daniel Paulino <danielpaulino030@gmail.com>
2015-11-02 17:05:03 -0200

    Root url

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,11 +1,12 @@
-import com.example.app.controllers.ProductsController
-import com.example.app.dbs.ProductTable
+import com.example.app.controllers._
+import com.example.app.tables.ProductTable
 import org.scalatra._
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     val collection = new ProductTable()
+    context.mount(new RootController, "/*")
     context.mount(new ProductsController(collection), "/products")
   }
 }

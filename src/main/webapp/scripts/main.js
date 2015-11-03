4f16606af1cc2e9fa003bf2c591ab7388ce84a9f
Daniel Paulino <danielpaulino030@gmail.com>
2015-11-03 09:45:33 -0200

    Better style

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -14,66 +14,68 @@ mod.factory("ProductsResource", ["$resource", function($resource) {
   });
 }]);
 
-mod.controller('Products', ["$scope", "$http", "$httpParamSerializer", "ProductsResource", function($scope, $http, $httpParamSerializer, ProductsResource) {
-  var createProduct = function() {
-    return {
-      name: "",
-      price: ""
+mod.controller('Products',
+  ["$scope", "$httpParamSerializer", "ProductsResource",
+  function($scope, $httpParamSerializer, ProductsResource) {
+    var createProduct = function() {
+      return {
+        name: "",
+        price: ""
+      };
     };
-  };
 
+    $scope.resetProduct = function() {
+      $scope.product = createProduct();
+    };
 
-  $scope.resetProduct = function() {
-    $scope.product = createProduct();
-  };
+    $scope.resetProduct();
+    $scope.products = ProductsResource.query();
 
-  $scope.resetProduct();
-  $scope.products = ProductsResource.query();
+    $scope.edit = function(product) {
+      $scope.product = product;
+    };
 
-  $scope.edit = function(product) {
-    $scope.product = product;
-  };
+    $scope.submit = function() {
+      event.preventDefault();
+      var product = $scope.product;
 
-  $scope.submit = function() {
-    event.preventDefault();
-    var product = $scope.product;
+      if ("id" in product) {
+        $scope.update(product);
+      } else {
+        $scope.save(product);
+      }
+    };
 
-    if ("id" in product) {
-      $scope.update(product);
-    } else {
-      $scope.save(product);
-    }
-  };
+    $scope.update = function(product) {
+      ProductsResource.update({ id: product.id }, $httpParamSerializer(product))
+        .$promise
+        .then(function() {
+          $scope.resetProduct();
+        });
+    };
 
-  $scope.update = function(product) {
-    ProductsResource.update({ id: product.id }, $httpParamSerializer(product))
-      .$promise
+    $scope.save = function(product) {
+      ProductsResource.save($httpParamSerializer(product))
+        .$promise
+        .then(function(response) {
+          $scope.products.unshift(response.product);
+        })
       .then(function() {
         $scope.resetProduct();
       });
-  };
+    };
 
-  $scope.save = function(product) {
-    ProductsResource.save($httpParamSerializer(product))
-      .$promise
-      .then(function(response) {
-        $scope.products.unshift(response.product);
-      })
-    .then(function() {
-      $scope.resetProduct();
-    });
-  };
-
-  $scope.remove = function(id) {
-    ProductsResource.delete({ id: id })
-      .$promise
-      .then(function() {
-        $scope.products = $scope.products.filter(function(prod) {
-          return prod.id !== id;
+    $scope.remove = function(id) {
+      ProductsResource.delete({ id: id })
+        .$promise
+        .then(function() {
+          $scope.products = $scope.products.filter(function(prod) {
+            return prod.id !== id;
+          });
         });
-      });
-  };
-}]);
+    };
+  }
+]);
 
 mod.run(["$http", function($http) {
   $http.defaults.headers.post["Content-Type"] = 'application/x-www-form-urlencoded';
